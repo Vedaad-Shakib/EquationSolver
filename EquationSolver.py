7ac9965feee002eceac92846abd5589855aa983e
Vedaad Shakib <vedaad799@gmail.com>
2014-09-27 23:29:40 -0700

    Added removeVariables function that cleans variables from equations to add after solve

--- EquationSolver.py
+++ EquationSolver.py
@@ -1,23 +1,64 @@
+import re
 from numpy import matrix
 from numpy import concatenate
 
-# must be in format <coefficient><letter>+<coefficient><letter>=<number>
-def solve(*args):
-    for i in args:
+# uses inverse matrix method
+# must be in format <number>+<number>=<number>; coefficients are assumed
+def solve(eqs):
+    for i in eqs:
         rightEq = matrix([int(j) for j in i.split("=")[0].split("+")])
-        leftEq = matrix([int (j) for j in i.split("=")[1]])
+        leftEq = matrix([int(i.split("=")[1])])
         if locals().has_key("rightSide"):
             rightSide = concatenate((rightSide, rightEq))
             leftSide = concatenate((leftSide, leftEq))
         else:
             rightSide = rightEq
             leftSide = leftEq
-    return rightSide.I*leftSide
+    return (rightSide.I*leftSide).T.tolist()[0] # transpose for 1 row matrix
         
 
-def cleanOperators(eq):
-    pass
+def cleanOperators(eqs):
+    return eqs
 
-def cleanFormat(eq):
-    pass
+# separates variable names and cleaned equations - reorders variables if not in correct order
+# note: these few lines of code took around 45 minutes to produce :P
+def removeVariables(eqs):
+    # puts all variables and coefficients in array variables, adding 1 if no coefficient exists
+    variables = [["1"+i if not i[0].isdigit() else i for i in re.findall("[0-9]*[a-zA-Z]+", eq)] for eq in eqs]
+
+    # gets the right side of the equation
+    rightCoefficients = [[i.split("=")[1]] for i in eqs]
+    
+    # sorts variables alphabetically
+    cleanedVariables = [sorted(i, key=lambda string: string[re.search("[a-z]", string).start():]) for i in variables]
+
+    # gets names of variables and coefficients of variables
+    leftCoefficients = [[re.sub("[a-zA-Z]", "", i) for i in j] for j in cleanedVariables]
+    variableNames = [re.sub("[0-9]", "", i) for i in cleanedVariables[0]]
+
+    # join right and left side for an equation that fits into the solve method above
+    equations = ["+".join(left)+"="+right[0] for left, right in zip(leftCoefficients, rightCoefficients)]
+
+    return variableNames, equations
+    
+         
+    
+
+def main():
+    equations = []
+    equation = " "
+    while equation != "":
+        equation = str(raw_input("Input equation " + str(len(equations)+1) + ": "))
+        equations.append(equation) if equation != "" else None
+
+    equations = cleanOperators(equations)
+    variables, equations = removeVariables(equations)
+    answers = solve(equations)
+
+    print "\n".join([str(variable)+": "+str(value) for variable, value in zip(variables, answers)])
+
+    
+
+
+main()
     
